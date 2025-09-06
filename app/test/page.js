ea8019a8a2c83df5bc07cf6b83dd4341bfc5554d
irony-man <shivamrai0401@gmail.com>
2025-09-06 22:34:19 +0530

    TEST

--- app/test/page.js
+++ app/test/page.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+
+export const metadata = {
+    title: "CricRadio",
+    description: "Dive",
+};
+
+
 export default function page() {
+
     return <div>page</div>;
 }
